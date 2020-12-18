49fb63e6f814a860ab2fb0211ea3741a657b0015
minhthuan1110 <thuanxieuxao2k@gmail.com>
2020-12-18 14:11:37 +0700

    edit serviceProvider https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,9 +23,9 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    // public function boot()
-    // {
+    public function boot()
+    {
     //    PaginationPaginator::useBootstrap();
             URL::forceScheme('https');
-    // }
+    }
 }
